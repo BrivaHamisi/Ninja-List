66fec315fbde52ebffa28f3ebad66487ebad14f3
BrivaHamisi <brivahamisi@gmail.com>
2024-10-18 16:02:27 +0300

    Layout Component

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-import Navbar from "@/comps/Navbar";
-import Footer from "@/comps/Footer";
 import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Home</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <Link href="/ninjas">See Ninja Listing</Link>
-      <Footer/>
     </div>
   );
 }
