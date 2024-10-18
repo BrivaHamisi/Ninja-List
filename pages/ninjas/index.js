efc6b113dd68560bfaa68008ab1324a1b86fca98
BrivaHamisi <brivahamisi@gmail.com>
2024-10-18 13:54:51 +0300

    Pages and Routes

---
+++ pages/ninjas/index.js
@@ -0,0 +1,10 @@
+const Ninjas = () => {
+    return ( 
+        <div>
+            <h1>Ninjas</h1>
+            <p>Welcome to the Ninja Academy!</p>
+        </div>
+     );
+}
+ 
+export default Ninjas;
