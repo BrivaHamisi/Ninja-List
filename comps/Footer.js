9b438f5c3809e1059a1ed73526959a7ab0e8df59
BrivaHamisi <brivahamisi@gmail.com>
2024-10-18 14:19:00 +0300

    Adding other Components'

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div>
+            <p>Copyright 2024 Ninja List</p>
+        </div>
+     );
+}
+ 
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,19 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import Navbar from "@/comps/navbar";
+import Footer from "@/comps/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <h1>Home</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+      <Footer/>
     </div>
   );
 }
