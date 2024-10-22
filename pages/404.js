7dde1e2a5968a46ba8438e03f3997e33a1cce8fe
BrivaHamisi <brivahamisi@gmail.com>
2024-10-22 16:41:00 +0300

    Page Redirect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        console.log('Use Effect Ran')
+            setTimeout(() => {
+                router.push('/')
+            }, 3000)
+        
+    }, []);
     return ( 
         <div className="not-found">
             <h1>404</h1>
